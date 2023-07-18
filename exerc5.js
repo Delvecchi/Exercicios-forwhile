b3a576de98fe98187cd53d57688f895313c9a402
Heitor-Delvecchi <heitordelveccho@hotmail.com>
2023-07-18 19:10:16 -0300

    Atualização exercicio 5

--- exerc5.js
+++ exerc5.js
@@ -2,12 +2,15 @@ console.log("Exercicio 5")
 let anterior = 0
 let fibonacci = 1
 
+for (let index = 0; index < 1; index++) {
+    let numero = 0
+    console.log(numero)
 
+    for (let index = 1; index < 10; index++) {
+        console.log(fibonacci)
+        let atual = fibonacci
+        fibonacci = anterior + atual
+        anterior = atual
 
-for (let index = 1; index <= 10; index++) {
-    console.log(fibonacci)
-    let atual = fibonacci
-    fibonacci = anterior + atual
-    anterior = atual
-    
+    }
 }
